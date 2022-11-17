c420cde25b13278397a743ecc6fa3d46c79a3235
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2022-11-17 19:32:53 +0330

    connect to postgres and write prisma

--- app/http/validators/admin/category.schema.js
+++ app/http/validators/admin/category.schema.js
@@ -2,7 +2,7 @@ const joi = require("@hapi/joi");
 const { MongoIDPattern } = require("../../../utils/constants");
 const addCategorySchema = joi.object({
         title : joi.string().min(3).max(30).error(new Error("عنوان دسته بندی صحیح نمیباشد")),
-        parent : joi.string().allow("").pattern(MongoIDPattern).allow("").error(new Error("شناسه ارسال شده صحیح نمیباشد"))
+        parent : joi.string().allow("").regex(MongoIDPattern).allow("").error(new Error("شناسه ارسال شده صحیح نمیباشد"))
 });
 const updateCategorySchema = joi.object({
     title : joi.string().min(3).max(30).error(new Error("عنوان دسته بندی صحیح نمیباشد"))
--- app/http/validators/user/auth.schema.js
+++ app/http/validators/user/auth.schema.js
@@ -1,9 +1,9 @@
 const joi = require("@hapi/joi")
 const getOTPSchema = joi.object({
-        mobile : joi.string().length(11).pattern(/^09[0-9]{9}$/).error(new Error("شماره موبایل وارد شده نادرست است"))
+        mobile : joi.string().length(11).regex(/^09[0-9]{9}$/).error(new Error("شماره موبایل وارد شده نادرست است"))
 });
 const checkOTPSchema = joi.object({
-    mobile : joi.string().length(11).pattern(/^09[0-9]{9}$/).error(new Error("شماره موبایل وارد شده نادرست است")),
+    mobile : joi.string().length(11).regex(/^09[0-9]{9}$/).error(new Error("شماره موبایل وارد شده نادرست است")),
     code : joi.string().min(4).max(6).error(new Error("کد ارسال شده صحیح نمیباشد"))
 });
 
