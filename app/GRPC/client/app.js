b4875d5d1290d0020c0fb257a05703bab44276de
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-03-19 15:11:03 +0330

    make changes

---
+++ app/GRPC/client/app.js
@@ -0,0 +1,53 @@
+const { AllRoutes } = require("./routes/index.routes");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const path = require("path");
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname , "..", "public")));
+app.use(AllRoutes);
+app.use( "/api-doc" , swaggerUI.serve , swaggerUI.setup(swaggerJsDoc({
+        swaggerDefinition : {
+            openapi: "3.0.0", 
+            info : {
+                title : "Boto Start Store",
+                version : "2.0.0",
+                description : "بزرگترین مرجع ارائه پکیج برنامه نویسی",
+                contact : {
+                    name : "Mohammad Aali",
+                    url : "http://github.com/Mohammad-a-tab",
+                    email : "mohammad.programmer.tab@gmail.com",
+                },
+                
+            },
+            servers : [
+                {
+                    url : "http://localhost:4000"
+                }
+            ],
+            
+        },
+        apis : ["./routes/**/*.js"]
+    }),
+    {explorer : true},
+));
+app.use((req, res, next) => {
+    return res.status(404).json({
+        statusCode : 404,
+        message : "Not Found"
+    })
+});
+app.use((err, req, res, next) => {
+    return res.status(err.status || 500).json({
+        data : null,
+        errors : [
+            statusCode = err.status || 500,
+            message = err.message
+        ]
+    })
+});
+app.listen(4000, () => {
+    console.log("Client running over port 4000");
+})
---
+++ app/GRPC/client/controllers/user.controller.js
@@ -0,0 +1,6 @@
+class UserController {
+
+}
+module.exports = {
+    UserController : new UserController()
+};
